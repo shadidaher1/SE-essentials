44b6ea7518155faa8fbb4c585e078df738ed4d74
shadidaher1 <shadidaher441@gmail.com>
2026-04-30 13:19:41 +0300

    Created Toy and Book models for Assignment 2

---
+++ src/Model/Toy.Model.ts
@@ -0,0 +1,84 @@
+import { Item, ItemCategory } from "./Item.Model";
+
+type ToyType = "Plush Toy" | "Building Blocks" | "Action Figure" | "Doll" | "Puzzle" | "Art Kit" | "Remote Car" | "Educational Toy" | "Board Game";
+type AgeGroup = "0-3" | "4-7" | "8-12" | "13+";
+type Brand = "FunTime" | "BuildSmart" | "Adventure Co." | "ToyLand" | "KidzJoy" | "Creative Minds" | "PlayMasters";
+type Material = "Fabric" | "Plastic" | "Foam" | "Wood" | "Metal";
+type BatteryRequired = "Yes" | "No";
+type Educational = "Yes" | "No";
+
+class Toy implements Item {
+    private orderId: string;
+    private toyType: ToyType;
+    private ageGroup: AgeGroup;
+    private brand: Brand;
+    private material: Material;
+    private batteryRequired: BatteryRequired;
+    private educational: Educational;
+    private price: number;
+    private quantity: number;
+
+    constructor(
+        orderId: string,
+        toyType: ToyType,
+        ageGroup: AgeGroup,
+        brand: Brand,
+        material: Material,
+        batteryRequired: BatteryRequired,
+        educational: Educational,
+        price: number,
+        quantity: number
+    ) {
+        this.orderId = orderId;
+        this.toyType = toyType;
+        this.ageGroup = ageGroup;
+        this.brand = brand;
+        this.material = material;
+        this.batteryRequired = batteryRequired;
+        this.educational = educational;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getCategory(): ItemCategory {
+        return ItemCategory.TOY;
+    }
+
+    getOrderId(): string {
+        return this.orderId;
+    }
+
+    getToyType(): ToyType {
+        return this.toyType;
+    }
+
+    getAgeGroup(): AgeGroup {
+        return this.ageGroup;
+    }
+
+    getBrand(): Brand {
+        return this.brand;
+    }
+
+    getMaterial(): Material {
+        return this.material;
+    }
+
+    getBatteryRequired(): BatteryRequired {
+        return this.batteryRequired;
+    }
+
+    getEducational(): Educational {
+        return this.educational;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+
+    getQuantity(): number {
+        return this.quantity;
+    }
+}
+
+export default Toy;
